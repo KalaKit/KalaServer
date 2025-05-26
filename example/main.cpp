369710e8215c6175d5d071ca001ac50ae4a3be60
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-05-26 18:58:22 +0300

    first pages + copying to exe path

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,8 @@
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
 
 #include "http_server.hpp"
 
@@ -8,17 +11,39 @@ using MiniWeb::Server;
 using std::string;
 using std::cout;
 using std::cin;
+using std::fstream;
+using std::stringstream;
+using std::filesystem::path;
+
+static string ServeFile(const string& fileName)
+{
+	path fullFilePath = path("static/" + fileName);
+	fstream file(fullFilePath);
+
+	stringstream buffer{};
+	buffer << file.rdbuf();
+	return buffer.str();
+}
 
 int main()
 {
+	cout << "Initializing MiniWeb...\n";
+
 	Server server(8080);
-	server.Route("/hello", [](const string&)
+
+	server.Route("/", [](const string&)
 		{
-			return string("<h1>Hello from MiniWeb!< / h1>");
+			return ServeFile("index.html");
 		});
+
+	server.Route("/about", [](const string&)
+		{
+			return ServeFile("about.html");
+		});
+
 	server.Run();
 
-	cout << "MiniWeb is open!\n";
+	cout << "MiniWeb is open! Press any key to exit...\n";
 	cin.get();
 
 	return 0;
--- script_regular_build.bat
+++ script_regular_build.bat
@@ -3,6 +3,9 @@
 set "PROJECT_ROOT=%~dp0"
 cd "%PROJECT_ROOT%"
 
+set "STATIC_ORIGIN=%PROJECT_ROOT%\example\static"
+set "STATIC_TARGET=%PROJECT_ROOT%\build-debug\example\static"
+
 :: Ensure Visual Studio environment is set up correctly
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" ||(
     echo [ERROR] Failed to set up Visual Studio environment.
@@ -25,6 +28,16 @@ if errorlevel 1 (
     pause
     exit /b 1
 )
+
+:: Copy static folder content to server folder
+if exist "%STATIC_TARGET%" (
+	rmdir /s /q "%STATIC_TARGET%"
+	echo Deleted old static folder folder.
+	echo.
+)
+
+mkdir "%STATIC_TARGET%"
+xcopy "%STATIC_ORIGIN%" "%STATIC_TARGET%\" /E /I /Y >nul
 
 pause
 exit /b 0
