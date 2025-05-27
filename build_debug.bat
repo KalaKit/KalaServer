c079d6ff1962b983da1ce6023aaaaea6dddc769e
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-05-27 08:36:42 +0300

    check for either buildtools or visual studio

--- build_debug.bat
+++ build_debug.bat
@@ -6,10 +6,16 @@ cd "%PROJECT_ROOT%"
 set "STATIC_ORIGIN=%PROJECT_ROOT%\example\static"
 set "STATIC_TARGET=%PROJECT_ROOT%\build-debug\example\static"
 
-:: Ensure Visual Studio environment is set up correctly
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" ||(
-    echo [ERROR] Failed to set up Visual Studio environment.
-	pause
+set "VS_MAIN=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+set "VS_SIDE=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+
+if exist "%VS_MAIN%" (
+    call "%VS_MAIN%"
+) else if exist "%VS_SIDE%" (
+    call "%VS_SIDE%"
+) else (
+    echo [ERROR] Could not find vcvars64.bat from either Visual Studio Community or Build Tools. Please download Visual Studio or Visual Studio Build Tools.
+    pause
     exit /b 1
 )
 
